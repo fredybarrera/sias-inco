f7bd81bcbf89154498804e6f12b335bea9715b5a
Fredys Barrera <fredy.barrera.a@gmail.com>
2021-01-06 13:51:35 -0300

    Se finaliza desarrollo widget para registrar notas de gestion

--- widgets/NotasGestion/Widget.js
+++ widgets/NotasGestion/Widget.js
@@ -38,7 +38,7 @@ define([
   "esri/SpatialReference",
   "esri/InfoTemplate", 
   "esri/geometry/Point", 
-  "jimu/loaderplugins/jquery-loader!https://code.jquery.com/jquery-3.5.1.min.js",
+  // "jimu/loaderplugins/jquery-loader!https://code.jquery.com/jquery-3.5.1.min.js",
 ],
 function(
   declare, 
@@ -64,8 +64,7 @@ function(
   Polygon,
   SpatialReference,
   InfoTemplate,
-  Point,
-  $){
+  Point){
   return declare(BaseWidget, {
     name: 'Notas de gestión',
     sias: null,
@@ -82,10 +81,8 @@ function(
 
       var html_infotemplate = this.getInfotemplate()
 
-      console.log('config: ', config);
-
       // Obtengo las notas de gestión registradas
-      let query = '/query?outFields=SIAs_Areas_SIA_ID_Gral&returnGeometry=false&where=1%3D1&f=pjson'
+      var query = '/query?outFields=SIAs_Areas_SIA_ID_Gral&returnGeometry=false&where=1%3D1&f=pjson';
       getRequest(config.urlBase + config.urlKeySias + query).then(
         lang.hitch(this, function(objRes) { 
           if(objRes.features.length > 0)
@@ -100,17 +97,93 @@ function(
         function(objErr) {
           console.log('request failed', objErr)
         }
-      )
+      );
+
+
+      // Obtengo los estados de gestión.
+      var query = '/query?outFields=*&where=1%3D1&f=pjson';
+      getRequest(config.urlBase + config.urlKeyEstadoGestion + query).then(
+        lang.hitch(this, function(objRes) { 
+          if(objRes.features.length > 0)
+          {
+            let html = '<option value="-1">[Seleccione]</option>';
+            arrayUtils.forEach(objRes.features, function(f) {
+              html += '<option value="'+ f.attributes.Estados_Gestion +'">'+ f.attributes.Estados_Gestion +'</option>'
+            }, this);
+            $('#sel-nota-gestion-estado-sia').html(html)
+          }
+        }),
+        function(objErr) {
+          console.log('request failed', objErr)
+        }
+      );
+
+      // Obtengo los profesionales inco.
+      var query = '/query?outFields=*&where=1%3D1&f=pjson';
+      getRequest(config.urlBase + config.urlKeyProfesionales + query).then(
+        lang.hitch(this, function(objRes) { 
+          if(objRes.features.length > 0)
+          {
+            let html = '<option value="-1">[Seleccione]</option>';
+            arrayUtils.forEach(objRes.features, function(f) {
+              html += '<option value="'+ f.attributes.Nombre_apellido +'">'+ f.attributes.Nombre_apellido +'</option>'
+            }, this);
+            $('#sel-nota-gestion-autor').html(html)
+          }
+        }),
+        function(objErr) {
+          console.log('request failed', objErr)
+        }
+      );
+
 
       $('#sel-nota-gestion-sia').change(function() {
         let id_sia = $(this).val();
         gLayer.clear();
 
-        // Aca deberia llamar a las notas de gestion anteriores de la SIA
-        // Falta guardar la nota de gestión.
+        // Obtengo el historial de nostas de gestión de la SIA.
+        var query = '/query?outFields=*&orderByFields=Fecha_Nota+desc&where=SIAIDGRAL2=\'' + id_sia + '\'&f=pjson';
+        getRequest(config.urlBase + config.urlKeyNotasDeGestion + query).then(
+          lang.hitch(this, function(objRes) { 
+            if(objRes.features.length > 0)
+            {
+              let html = '<table class="table table-hover">';
+              html += '<thead>';
+              html += '<tr>';
+              html += '<th scope="col">SIA</th>';
+              html += '<th scope="col">Fecha ingreso nota</th>';
+              html += '<th scope="col">Estado de gestión</th>';
+              html += '<th scope="col">Nota</th>';
+              html += '<th scope="col">Nota registrada por</th>';
+              html += '</tr>';
+              html += '</thead>';
+              html += '<tbody>';
+              arrayUtils.forEach(objRes.features, function(f) {
+                var d = new Date(f.attributes.Fecha_Nota)
+                // var options = { year: '2-digit', month: '2-digit', day: '2-digit' };
+                var options = {  dateStyle: 'medium', timeStyle: 'medium' };
+                var datetime = d.toLocaleString("es-CL", options);
+                html += '<tr>';
+                html += '<td>' + f.attributes.SIA_ID_LOCAL + '</td>';
+                html += '<td>' + datetime + '</td>';
+                html += '<td>' + f.attributes.Estado_gestion + '</td>';
+                html += '<td>' + f.attributes.Comentario + '</td>';
+                html += '<td>' + f.attributes.Nombre_apellido + '</td>';
+                html += '</tr>';
+              }, this);
 
+              html += '</tbody>';
+              html += '</table>';
+              $('#div-notas-registradas').html(html)
+            }
+          }),
+          function(objErr) {
+            console.log('request failed', objErr)
+          }
+        );
 
-        let query = '/query?outFields=*&returnGeometry=true&where=SIAs_Areas_SIA_ID_Gral=\'' + id_sia + '\'&f=pjson'
+        
+        var query = '/query?outFields=*&returnGeometry=true&where=SIAs_Areas_SIA_ID_Gral=\'' + id_sia + '\'&f=pjson'
         getRequest(config.urlBase + config.urlKeySias + query).then(
           lang.hitch(this, function(objRes) { 
             if(objRes.features.length > 0)
@@ -119,30 +192,40 @@ function(
               let geom = objRes.features[0].geometry
               let sr = objRes.spatialReference
 
+              var d = new Date(data.Dat_SIAs_Fecha_Solicitud)
+              var options = {  dateStyle: 'medium', timeStyle: 'medium' };
+              var datetime = d.toLocaleString("es-CL", options);
+
               $("#nota-gestion-detalle-epc").val(data.Dat_SIAs_SIA_EPC);
               $("#nota-gestion-detalle-id-sia").val(data.Dat_SIAs_SIA_ID_LOCAL);
               $("#nota-gestion-detalle-sia-origen").val(data.Dat_SIAs_SIA_Origen);
-              $("#nota-gestion-detalle-fecha-solicitud").val(data.Dat_SIAs_Fecha_Solicitud);
+              $("#nota-gestion-detalle-fecha-solicitud").val(datetime);
               $("#txta-nota-gestion-detalle-texto").text(data.Dat_SIAs_Area_Solicitada);
+              $("#txta-nota-gestion-comentario-texto").text(data.Dat_SIAs_Comentario);
+              $("#nota-gestion-estado-gestion").val(data.Dat_SIAs_Estados_Gestion);
               $("#nota-gestion-detalle-registrada").val(data.Resgistrado_por);
               $("#nota-gestion-detalle-solicitada").val(data.ID_Solicitante);
+              $("#nota-gestion-m2").val(data.SIAs_Areas_Area_m2);
+              $("#nota-gestion-id-sia-gral").val(data.SIAs_Areas_SIA_ID_Gral);
+              $("#nota-gestion-detalle-objectid").val(data.OBJECTID);
 
               var sfs = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
                 new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
                 new Color([255,0,0]), 2),new Color([255,255,0,0.25])
               );
 
+
               var attr = {
                 "Dat_SIAs_SIA_EPC": data.Dat_SIAs_SIA_EPC,
                 "Dat_SIAs_SIA_ID_LOCAL": data.Dat_SIAs_SIA_ID_LOCAL,
                 "Dat_SIAs_SIA_Origen": data.Dat_SIAs_SIA_Origen,
-                "Dat_SIAs_Fecha_Solicitud": data.Dat_SIAs_Fecha_Solicitud,
+                "Dat_SIAs_Fecha_Solicitud": datetime,
                 "Dat_SIAs_Area_Solicitada": data.Dat_SIAs_Area_Solicitada,
                 "Resgistrado_por": data.Resgistrado_por,
                 "ID_Solicitante": data.ID_Solicitante,
+                "SIAs_Areas_SIA_ID_Gral": data.SIAs_Areas_SIA_ID_Gral
               };
 
-              console.log('geom.rings: ', geom.rings);
               var latLon
               var infoTemplate = new InfoTemplate("SIA", html_infotemplate);
               var polygon = new Polygon(new SpatialReference(sr));
@@ -153,7 +236,7 @@ function(
               var graphic = new Graphic(polygon, sfs, attr, infoTemplate);
               gLayer.add(graphic);
               point = new Point(latLon[0], latLon[1]);
-							map.centerAndZoom(point, 15);
+							map.centerAt(point);
             }
           }),
           function(objErr) {
@@ -166,42 +249,130 @@ function(
 
     _onclickEnviar: function () {
       var deferred = new Deferred();
+      var showMessage = this.showMessage;
 
-      console.log('acaaaaaaaaa');
+      this.getData().then(
+        lang.hitch(this, function(data) { 
+          strData = JSON.stringify([data])
+          console.log('data: ', data);
+          var objectid = $('#nota-gestion-detalle-objectid').val();
+          var estadoGestion = data.attributes.Estado_gestion
+          this.postRequest(this.config.urlBase + this.config.urlKeyNotasDeGestion + '/applyEdits', strData, 'adds').then(
+            lang.hitch(this, function(objRes) { 
+              console.log('objRes: ', objRes)
+              if (objRes.addResults[0].success === true)
+              {
+                // Actualizo el estado de la SIA.
+                var data = {};
+                var attributes = {};
+                attributes['Dat_SIAs_Estados_Gestion'] = estadoGestion
+                attributes['OBJECTID'] = parseInt(objectid);
+                data['attributes'] = attributes;
+                strData = JSON.stringify([data])
 
-      // this.getData().then(
-      //   lang.hitch(this, function(data) { 
-      //     strData = JSON.stringify([data])
-      //     this.postRequest(this.config.urlBase + this.config.urlKeySias + '/applyEdits', strData).then(
-      //       lang.hitch(this, function(objRes) { 
-      //         console.log('objRes: ', objRes)
-      //         if (objRes.addResults[0].success === true)
-      //         {
-      //           this.showMessage('Sia ingresada exitosamente')
-      //           deferred.resolve(objRes);
-      //         } else {
-      //           msg = objRes.addResults[0].error.description
-      //           this.showMessage('Error al enviar la información: ' + msg, 'error')
-      //         }
-      //       }),
-      //       function(objErr) {
-      //         deferred.resolve([]);
-      //       }
-      //     )
-      //     return deferred.promise;
-      //   }),
-      //   lang.hitch(this, function(strError) {
-      //     console.log('request failed', strError);
-      //     this.showMessage(strError, 'error')
-      //   })
-      // );
+                this.postRequest(this.config.urlBase + this.config.urlKeySias + '/applyEdits', strData, 'updates').then(
+                  lang.hitch(this, function(objRes) { 
+                    console.log('objRes: ', objRes)
+                    if (objRes.updateResults[0].success === true)
+                    {
+                      showMessage('Nota de gestión ingresada exitosamente');
+                      deferred.resolve(objRes);
+                    } else {
+                      msg = objRes.addResults[0].error.description
+                      showMessage('Error al enviar la información: ' + msg, 'error')
+                    }
+                  }),
+                  function(objErr) {
+                    deferred.resolve([]);
+                  }
+                );
+              } else {
+                msg = objRes.addResults[0].error.description
+                showMessage('Error al enviar la información: ' + msg, 'error')
+              }
+            }),
+            function(objErr) {
+              deferred.resolve([]);
+            }
+          );
+          return deferred.promise;
+        }),
+        lang.hitch(this, function(strError) {
+          console.log('request failed', strError);
+          this.showMessage(strError, 'error')
+        })
+      );
+    },
+
+
+    getData: function () {
+      var deferred = new Deferred();
+      var data = {};
+      var attributes = {};
+
+      // Valido que se elija una SIA
+      var sia_gral = $('#sel-nota-gestion-sia option:selected').val();
+      if (sia_gral == '-1' || sia_gral == '')
+      {
+        deferred.reject('Debe seleccionar una SIA')
+      } else {
+        attributes['SIAIDGRAL2'] = sia_gral
+      }
+
+      //Valido que ingrese la fecha de la solicitud
+      var fechaSolicitud = $('#txt-nota-gestion-fecha').val();
+      let datetime = new Date(fechaSolicitud).getTime();
+
+      if (fechaSolicitud == '')
+      {
+        deferred.reject('Debe seleccionar una fecha de solicitud')
+      } else {
+        attributes['Fecha_Nota'] = datetime;
+      }
+
+      //Valido que ingrese el estado de la nota de gestion.
+      var estado = $('#sel-nota-gestion-estado-sia option:selected').val()
+      if (estado == '-1' || estado == '')
+      {
+        deferred.reject('Debe seleccionar un estado actual de gestión')
+      } else {
+        attributes['Estado_gestion'] = estado;
+      }
+
+      //Valido que ingrese un comentario
+      var comentario = $('#txta-nota-gestion-texto').val()
+      if (comentario == '')
+      {
+        deferred.reject('Debe ingresar una nota de gestión')
+      } else {
+        attributes['Comentario'] = comentario
+      }
+
+      //Valido que ingrese un profesional inco.
+      var profesional = $('#sel-nota-gestion-autor option:selected').val()
+      if (profesional == '-1' || profesional == '')
+      {
+        deferred.reject('Debe seleccionar un autor')
+      } else {
+        attributes['Nombre_apellido'] = profesional;
+      }
+
+      var id_sia_gral = $('#nota-gestion-detalle-id-sia').val()
+      attributes['SIA_ID_LOCAL'] = id_sia_gral;
+
+
+      data['attributes'] = attributes;
+      deferred.resolve(data);
+			return deferred.promise;
     },
 
     getInfotemplate: function () {
       var html_infotemplate = '<table cellspacing="0" cellpadding="0" style="border: none;"><tbody>'
+      html_infotemplate += '<tr><td><b>SIA ID Gral: </b></td>';
+      html_infotemplate += '<td>${SIAs_Areas_SIA_ID_Gral}</td></tr>';
       html_infotemplate += '<tr><td><b>EPC: </b></td>';
       html_infotemplate += '<td>${Dat_SIAs_SIA_EPC}</td></tr>';
-      html_infotemplate += '<tr><td><b>ID SIA: </b></td>';
+      html_infotemplate += '<tr><td><b>SIA ID Local: </b></td>';
       html_infotemplate += '<td>${Dat_SIAs_SIA_ID_LOCAL}</td></tr>';
       html_infotemplate += '<tr><td><b>SIA Origen: </b></td>';
       html_infotemplate += '<td>${Dat_SIAs_SIA_Origen}</td></tr>';
@@ -255,13 +426,13 @@ function(
       return deferred.promise;
     },
 
-    postRequest: function (url, data) {
+    postRequest: function (url, data, type) {
       try{
         var deferred = new Deferred();
         
         let formData = new FormData();
         formData.append('f', 'json');
-        formData.append('adds', data);
+        formData.append(type, data);
 
         let fetchData = {
             method: 'POST',
